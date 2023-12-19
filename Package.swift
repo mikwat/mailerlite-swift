ae1e04f731105c6230963f5ded41740f64bc0f1c
Michael Watts <103135+mikwat@users.noreply.github.com>
2023-12-19 13:29:59 -0800

    Change minimum platform requirements

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "MailerLiteApiClient",
+    platforms: [
+        .macOS(.v10_15), .iOS(.v15), .tvOS(.v15)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
